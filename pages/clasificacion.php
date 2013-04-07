076cc0ef7713ce61e7ef3a9694fb2f177249eac1
altapia <altapia@gmail.com>
2013-04-07 19:46:46 +0200

    clasificaciones por fases

--- pages/clasificacion.php
+++ pages/clasificacion.php
@@ -16,7 +16,7 @@
 
 	<!-- Google Charts -->
 <?
-include "../pages/conexion_bd.php";
+	include "../pages/conexion_bd.php";
 ?>
 
 	<script type="text/javascript" src="https://www.google.com/jsapi"></script>
@@ -25,13 +25,6 @@ include "../pages/conexion_bd.php";
       google.setOnLoadCallback(drawChart);
       function drawChart() {
 
-      	<?
-      	$result2=mysql_query("select apos, ap.nombre, fec, sum(neto) as neto from
-	(SELECT p.apostante as apos, p.fecha as fec, round(if(a.acertada=1,a.apostado*a.cotizacion-a.apostado,a.apostado*-1),2) as neto
-	FROM apuestas a, partidos p WHERE a.partido = p.id and a.acertada>0) as culo,
-	apostantes ap where apos = ap.id
-	group by apos, fec order by fec, apos;");
-		?>
 			var data = google.visualization.arrayToDataTable([
           		['Fecha', 'Tapia', 'Nano', 'Rulo', 'Zato', 'Poles', 'Borja', 'Matute', 'Lucho', 'Total por día'],
 			['Inicio', 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -50,6 +43,52 @@ include "../pages/conexion_bd.php";
 				    8 => 0
 				);
 			$total_dia=0;
+		
+		
+		
+		$queryIni="select 
+      			apos, 
+      			ap.nombre, 
+      			fec, 
+      			sum(neto) as neto 
+      		from
+				(
+				SELECT 	
+					p.apostante as apos, 
+					p.fecha as fec, 
+					round(if(a.acertada=1,a.apostado*a.cotizacion-a.apostado,a.apostado*-1),2) as neto
+				FROM 
+					apuestas a, 
+					partidos p 
+				WHERE 
+					a.partido = p.id and 
+					a.acertada>0";
+		
+		$queryFase ="";
+		if($_GET["fase"]=="grupos"){
+			$queryFase=" and p.fase like 'GRUPO%' ";
+		}else if($_GET["fase"]=="octavos"){
+			$queryFase=" and p.fase = 'OCTAVOS' ";
+		}if($_GET["fase"]=="cuartos"){
+			$queryFase=" and p.fase = 'CUARTOS' ";
+		}if($_GET["fase"]=="semi"){
+			$queryFase=" and p.fase = 'SEMI' ";
+		}if($_GET["fase"]=="final"){
+			$queryFase=" and p.fase = 'FINAL' ";
+		}
+		
+		$queryFin=") as culo,
+				apostantes ap 
+			where 
+				apos = ap.id
+			group by apos, 
+					fec 
+			order by 
+				fec, 
+				apos;";
+		
+      		$result2=mysql_query($queryIni.$queryFase.$queryFin);
+		
 			 while ($row=mysql_fetch_array($result2)){
 				$apostante=$row["apos"];
 				if($i==0){
@@ -114,112 +153,103 @@ include "../pages/conexion_bd.php";
         chart.draw(data, options2);
 
 /************/
-	<?
-	$result2=mysql_query("select apos, ap.nombre, fec, sum(neto) as neto from
-	(SELECT p.apostante as apos, p.fecha as fec, round(if(a.acertada=1,a.apostado*a.cotizacion-a.apostado,a.apostado*-1),2) as neto
-	FROM apuestas a, partidos p WHERE a.partido = p.id and a.acertada>0) as culo,
-	apostantes ap where apos = ap.id
-	group by apos, fec order by fec, apos;");
-	?>
+
 	
 	var data2 = google.visualization.arrayToDataTable([
           		['Fecha', 'Tapia', 'Nano', 'Rulo', 'Zato', 'Poles', 'Borja', 'Matute', 'Lucho', 'Total por día'],
 							['Inicio', 0, 0, 0, 0, 0, 0, 0, 0, 0],
-		<?
-
-			$i=0;
-			$apostante=0;
-			$array = array(
-				    1 => 0,
-				    2 => 0,
-				    3 => 0,
-				    4 => 0,
-				    5 => 0,
-				    6 => 0,
-				    7 => 0,
-				    8 => 0
-				);
-			$total_dia=0;
-			 /*while ($row=mysql_fetch_array($result2)){
-				$apostante=$apostante+1;
-				if($i==0){
-			 		$fecha_ini=$row["fec"];
-					$date = date_create($row["fec"]);
-					echo '[\''.date_format($date, 'd/m/Y').'\'';
-				}
-				$i=1;
-
-				if($fecha_ini==$row["fec"]){
-					$total_dia=$total_dia+$row["neto"];
-					$array[$apostante]=$row["neto"];
-					echo ','.$array[$apostante];
-				}else{
-					echo '],';
-					$date = date_create($row["fec"]);
-					echo '[\''.date_format($date, 'd/m/Y').'\'';
-
-					$fecha_ini=$row["fec"];
-					$total_dia=$total_dia+$row["neto"];
-					$array[$apostante]=$row["neto"];
-					echo ','.$array[$apostante];
-				}
-				if($apostante==8){
-					$apostante=0;
-					echo ','.$total_dia;
-					$total_dia=0;
-					}
-			}mysql_free_result($result2);
-			if($apostante>0){
-				for (;$apostante<8;$apostante++){
-					echo ',0';
-				}
-				echo ','.$total_dia;
+<? 
+		$i = 0;
+		$apostante = 0;
+		$array = array(1 => 0, 2 => 0, 3 => 0, 4 => 0, 5 => 0, 6 => 0, 7 => 0, 8 => 0);
+		$total_dia = 0;
+		
+		$queryIni="select 
+				apos, 
+				ap.nombre, 
+				fec, 
+				sum(neto) as neto 
+			from
+				(
+				SELECT 
+					p.apostante as apos, 
+					p.fecha as fec, 
+					round(if(a.acertada=1,a.apostado*a.cotizacion-a.apostado,a.apostado*-1),2) as neto
+				FROM 
+					apuestas a, 
+					partidos p 
+				WHERE 
+					a.partido = p.id and 
+					a.acertada>0";
+		$queryFase ="";
+		if($_GET["fase"]=="grupos"){
+			$queryFase=" and p.fase like 'GRUPO%' ";
+		}else if($_GET["fase"]=="octavos"){
+			$queryFase=" and p.fase = 'OCTAVOS' ";
+		}if($_GET["fase"]=="cuartos"){
+			$queryFase=" and p.fase = 'CUARTOS' ";
+		}if($_GET["fase"]=="semi"){
+			$queryFase=" and p.fase = 'SEMI' ";
+		}if($_GET["fase"]=="final"){
+			$queryFase=" and p.fase = 'FINAL' ";
+		}
+		$queryFin=") as culo,
+				apostantes ap 
+			where 
+				apos = ap.id
+			group by 
+				apos, 
+				fec 
+			order by 
+				fec, 
+				apos;";
+		
+		$result2=mysql_query($queryIni.$queryFase.$queryFin);
+		
+		while ($row = mysql_fetch_array($result2)) {
+			$apostante = $row["apos"];
+			if ($i == 0) {
+				$fecha_ini = $row["fec"];
+				$date = date_create($row["fec"]);
+				echo '[\'' . date_format($date, 'd/m/Y') . '\'';
 			}
-			echo ']]);';*/
- while ($row=mysql_fetch_array($result2)){
-				$apostante=$row["apos"];
-				if($i==0){
-			 		$fecha_ini=$row["fec"];
-					$date = date_create($row["fec"]);
-					echo '[\''.date_format($date, 'd/m/Y').'\'';
-				}
-				$i=1;
-				
-				if($fecha_ini==$row["fec"]){
-					$total_dia=$total_dia+$row["neto"];
-					$array[$apostante]=$row["neto"];
-				}else{
-					for($ii=1;$ii<9;$ii++){
-						if($array[$ii]==null){
-							echo ',0';
-						}else{
-							echo ','.$array[$ii];
-						}
-					}
-					for($ii=1;$ii<9;$ii++){
-						$array[$ii]=0;
-					}
-					
-					echo ','.$total_dia;
-					echo '],';
-					$total_dia=0;
-					$date = date_create($row["fec"]);
-					echo '[\''.date_format($date, 'd/m/Y').'\'';
+			$i = 1;
 
-					$fecha_ini=$row["fec"];
-					$total_dia=$total_dia+$row["neto"];
-					$array[$apostante]=$row["neto"];
-				}
-			}mysql_free_result($result2);
-			for($ii=1;$ii<9;$ii++){
-						if($array[$ii]==null){
-							echo ',0';
-						}else{
-							echo ','.$array[$ii];
-						}
+			if ($fecha_ini == $row["fec"]) {
+				$total_dia = $total_dia + $row["neto"];
+				$array[$apostante] = $row["neto"];
+			} else {
+				for ($ii = 1; $ii < 9; $ii++) {
+					if ($array[$ii] == null) {
+						echo ',0';
+					} else {
+						echo ',' . $array[$ii];
 					}
-					echo ','.$total_dia;		
-			echo ']]);';
+				}
+				for ($ii = 1; $ii < 9; $ii++) {
+					$array[$ii] = 0;
+				}
+
+				echo ',' . $total_dia;
+				echo '],';
+				$total_dia = 0;
+				$date = date_create($row["fec"]);
+				echo '[\'' . date_format($date, 'd/m/Y') . '\'';
+
+				$fecha_ini = $row["fec"];
+				$total_dia = $total_dia + $row["neto"];
+				$array[$apostante] = $row["neto"];
+			}
+		}mysql_free_result($result2);
+		for ($ii = 1; $ii < 9; $ii++) {
+			if ($array[$ii] == null) {
+				echo ',0';
+			} else {
+				echo ',' . $array[$ii];
+			}
+		}
+		echo ',' . $total_dia;
+		echo ']]);';
     ?>
 
 	var options = {
@@ -233,8 +263,8 @@ include "../pages/conexion_bd.php";
         var chart = new google.visualization.ComboChart(document.getElementById('chart_div3'));
         chart.draw(data2, options);
 
-
       }
+      
     </script>
 	
 </head>
@@ -259,7 +289,7 @@ include "../pages/conexion_bd.php";
 							<li ><a href="../pages/final.php">Final</a></li>-->
 						</ul>
 					</li>
-			<li><a href="../pages/apuestas.php">Apuestas</a></li>
+					<li><a href="../pages/apuestas.php">Apuestas</a></li>
                     <li class="active"><a href="#">Clasificación</a></li>
 				</ul>
 			</div>
@@ -268,24 +298,90 @@ include "../pages/conexion_bd.php";
 
 
 <? 
-include_once "../pages/iconos_usuarios.php";
+	include_once "../pages/iconos_usuarios.php";
 ?>
-			<div class="span10">
-				<div class="container">
-					<h2>Clasificación de apostantes</h2>
 
+<div class="span10">
+	<div class="container">
+		<h2>Clasificación de apostantes</h2>
+		
+		<ul class="nav nav-tabs">
+			<li <? if($_GET["fase"]=="total" || $_GET["fase"]=="" ){ echo 'class="active"';}?>>
+				<a href="./clasificacion.php?fase=total">Total</a>
+			</li>
+			<li <? if($_GET["fase"]=="grupos"){ echo 'class="active"';}?>>
+				<a href="./clasificacion.php?fase=grupos">Grupos</a>
+			</li>
+			<li <? if($_GET["fase"]=="octavos"){ echo 'class="active"';}?>>
+				<a href="./clasificacion.php?fase=octavos">Octavos</a>
+			</li>
+			<li <? if($_GET["fase"]=="cuartos"){ echo 'class="active"';}?>>
+				<a href="./clasificacion.php?fase=cuartos">Cuartos</a>
+			</li>
+			<li <? if($_GET["fase"]=="semi"){ echo 'class="active"';}?>>
+				<a href="./clasificacion.php?fase=semi">Semifinal</a>
+			</li>
+			<li <? if($_GET["fase"]=="final"){ echo 'class="active"';}?>>
+				<a href="./clasificacion.php?fase=final">Final</a>
+			</li>
+		</ul>
 
-<!-- FASE DE GRUPOS-->
-<h3>Fase de grupos</h3>
+<? 
+	
+	if($_GET["fase"]=="grupos"){ echo '<h3>Fase de grupos</h3>';}
+	else if($_GET["fase"]=="octavos"){ echo '<h3>Octavos de final</h3>';}
+	else if($_GET["fase"]=="cuartos"){ echo '<h3>Cuartos de final</h3>';}
+	else if($_GET["fase"]=="semi"){ echo '<h3>Semifinal</h3>';}
+	else if($_GET["fase"]=="final"){ echo '<h3>Final</h3>';}
+	else echo '<h3>Todas las fases</h3>';
 
-<?
 	
 	//Clasificación fase de grupos
-	$result=mysql_query("Select *,  @rownum:=@rownum+1 AS pos from (
-select culo.apos, ap.nombre, round(sum(culo.ganancia),2) as neto from 
-(select p.apostante as apos , if(a.acertada=1, a.apostado*a.cotizacion-a.apostado,a.apostado*-1)  as ganancia  
-from partidos p, apuestas a where p.id = a.partido and a.acertada>0)  as culo, apostantes ap where ap.id=culo.apos group by apos order by neto desc)
-puntuacio, (SELECT @rownum:=0) r;");			
+	$queryParte1=
+		"SELECT 
+				*,  
+				@rownum:=@rownum+1 AS pos 
+		FROM 
+			(
+			SELECT 
+				culo.apos, 
+				ap.nombre, 
+				round(sum(culo.ganancia),2) as neto 
+			FROM
+				(
+				SELECT 
+					p.apostante as apos , 
+					if(a.acertada=1, a.apostado*a.cotizacion-a.apostado,a.apostado*-1)  as ganancia  
+				FROM 
+					partidos p, 
+					apuestas a 
+				WHERE 
+					p.id = a.partido and 
+					a.acertada>0";
+				
+	$queryParte2=")  as culo, 
+				apostantes ap 
+			WHERE 
+				ap.id=culo.apos 
+			GROUP BY apos 
+			ORDER BY neto desc
+			) puntuacio, (SELECT @rownum:=0) r;";
+	
+	$queryFase ="";
+	if($_GET["fase"]=="grupos"){
+		$queryFase=" and p.fase like 'GRUPO%' ";
+	}else if($_GET["fase"]=="octavos"){
+		$queryFase=" and p.fase = 'OCTAVOS' ";
+	}if($_GET["fase"]=="cuartos"){
+		$queryFase=" and p.fase = 'CUARTOS' ";
+	}if($_GET["fase"]=="semi"){
+		$queryFase=" and p.fase = 'SEMI' ";
+	}if($_GET["fase"]=="final"){
+		$queryFase=" and p.fase = 'FINAL' ";
+	}
+			
+	$result=mysql_query($queryParte1.$queryFase.$queryParte2);
+				
 ?>
 <br>
 <table class="table table-condensed">
@@ -297,40 +393,44 @@ puntuacio, (SELECT @rownum:=0) r;");
     </tr>
 </thead>
 <tbody>
+	
 <? 
-$tot_gan=0;
-  while ($row=mysql_fetch_array($result))
-	{
-	$tot_gan=$tot_gan+$row["neto"];
-	echo '<tr>';
-	echo '<td style="text-align:center">'.$row["pos"];
-	echo '</td>';
-	echo '<td style="text-align:center"> <img src="../images/'.get_ico_usuario($row["nombre"]).'" height="20"/> '.$row["nombre"].'</td>';
-	echo '<td style="text-align:center">'.$row["neto"].'&euro;</td>';
-	echo '</tr>	';
+	
+	$tot_gan=0;
+	
+	while ($row=mysql_fetch_array($result)){
+		$tot_gan=$tot_gan+$row["neto"];
+		echo '<tr>';
+		echo '<td style="text-align:center">'.$row["pos"];
+		echo '</td>';
+		echo '<td style="text-align:center"> <img src="../images/'.get_ico_usuario($row["nombre"]).'" height="20"/> '.$row["nombre"].'</td>';
+		echo '<td style="text-align:center">'.$row["neto"].'&euro;</td>';
+		echo '</tr>	';
 	}
+	
 	mysql_free_result($result);
-	 echo '<tr>';
+	
+	echo '<tr>';
 	echo '<td colspan="2" style="text-align:center"><strong>Ganancia Total</strong></td>';
 	echo '<td style="text-align:center"><strong>'.$tot_gan.'&euro;</strong></td>';
 	echo '</tr>     ';
+	
 ?>
-  </tbody>
-</table>
+
+  			</tbody>
+		</table>
                   
                  
-<div id="chart_div" style="width: 900px; height: 500px;"></div>        
-<div id="chart_div2" style="width: 900px; height: 500px;"></div>
-<div id="chart_div3" style="width: 900px; height: 500px;"></div>
-</div>
-				<!--container -->
-			</div>
-		</div>
+		<div id="chart_div" style="width: 900px; height: 500px;"></div>        
+		<div id="chart_div2" style="width: 900px; height: 500px;"></div>
+		<div id="chart_div3" style="width: 900px; height: 500px;"></div>
 	</div>
+</div>
 
-	<script
-		src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
-	<script src="../bootstrap_new/js/bootstrap.min.js"></script>
+
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+<script src="../bootstrap_new/js/bootstrap.min.js"></script>
+
 </body>
 </html>
 
